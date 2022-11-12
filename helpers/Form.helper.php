e5f621a3dd6f0726ec84921900d5290c89d74272
Julius Derigs <julius.derigs@julius-derigs.de>
2022-11-12 02:39:24 +0100

    Completely rebuilding the FormHelper

--- helpers/Form.helper.php
+++ helpers/Form.helper.php
@@ -1,104 +1,118 @@
 <?php
+/*
+ * The documentation for this helper can be found in documentation/helpers/FormHelper.md
+ */
+
 class FormHelper {
-    public static function is_post_submitted() {
-        return (isset($_POST) && !empty($_POST)) ? true : false;
-    }
-
-    /*
-     * The array must be multidimensional
-     *
-     * $inputs = [
-     *  [
-     *      'type' => 'text',
-     *      'name' => 'input-name',
-     *      'id' => 'input-id',
-     *      'title' => 'input-title',
-     *      'placeholder' => 'input-placeholder',
-     *      'maxlength' => '255',
-     *      'class' => 'input-class-1 input-class-2',
-     *      'autocomplete' => 'off',
-     *      'value' => 'Some Value'
-     *  ],
-     *  [
-     *      'type' => 'text',
-     *      'name' => 'input-name',
-     *      'id' => 'input-id',
-     *      'title' => 'input-title',
-     *      'placeholder' => 'input-placeholder',
-     *      'maxlength' => '255',
-     *      'class' => 'input-class-1 input-class-2',
-     *      'autocomplete' => 'off',
-     *      'required' => 'required',
-     *      'value' => 'Some Value',
-     *      'disabled' => 'disabled'
-     *  ]
-     * ];
-     */
-
-    /*
-     * Currently not available with selects and labels
-     * The arrays inputs and buttons must be multidimensional
-     */
-    public static function build_form($form_information, $inputs, $buttons) {
-        $form_string = '';
-
-        /* Exit if given params aren't arrays */
-        if (!is_array($form_information) || !is_array($inputs) || !is_array($buttons)) {
-            if (ENVIRONMENT === 'development') {
-                echo LANG['DevelopmentErrors']['FormHelper']['BuildForm']['NotAnArray'];
-                die();
+    public static function is_form_submitted(string $type = 'post') :bool {
+        if (strtolower($type) === 'post') {
+            $submitted = isset($_POST) && !empty($_POST);
+        }
+        elseif (strtolower($type) === 'get') {
+            $submitted = isset($_GET) && !empty($_GET);
+        }
+        else {
+            if (ApplicationHelper::is_development()) {
+                echo 'Error: ' . LANG['DevelopmentErrors']['FormHelper']['IsFormSubmitted']['UnknownType'];
+                exit();
             }
             else {
-                ApplicationHelper::redirerct_to_error();
+                ApplicationHelper::redirect_to_error();
+                exit();
             }
         }
 
-        $action = (empty($form_information['action'])) ? '' : $form_information['action'];
-        $method = (empty($form_information['method'])) ? 'POST' : $form_information['method'];
-        $form_classes = (empty($form_information['classes'])) ? '' : $form_information['classes'];
+        return $submitted;
+    }
 
-        /* Build the form tag */
-        $form_string .= "<form action='$action' method='$method' class='$form_classes'>";
+    public static function build_input(array $input) :string {
+        $html = '';
+        $html .= '<input ';
+        $html .= empty($input['type']) ? '' : 'type="' . $input['type'] . '" ';
+        $html .= empty($input['name']) ? '' : 'name="' . $input['name'] . '" ';
+        $html .= empty($input['ids']) ? '' : 'id="' . $input['ids'] . '" ';
+        $html .= empty($input['classes']) ? '' : 'class="' . $input['classes'] . '" ';
+        $html .= empty($input['title']) ? '' : 'title="' . ApplicationHelper::make_string_safe($input['title']) . '" ';
+        $html .= empty($input['placeholder']) ? '' : 'placeholder="' . ApplicationHelper::make_string_safe($input['placeholder']) . '" ';
+        $html .= empty($input['maxlength']) ? '' : 'maxlength="' . intval($input['maxlength']) . '" ';
+        $html .= empty($input['autocomplete']) ? '' : 'autocomplete="' . $input['autocomplete'] . '" ';
+        $html .= empty($input['value']) ? '' : 'value="' . ApplicationHelper::make_string_safe($input['value']) . '" ';
+        $html .= empty($input['disabled']) ? '' : 'disabled ';
+        $html .= empty($input['required']) ? '' : 'required ';
+        $html .= empty($input['readonly']) ? '' : 'readonly ';
+        $html .= '/>';
 
-        $default_input_type = 'text';
-        $default_max_length = 255;
+        return $html;
+    }
 
-        /* Build the input fields */
-        foreach ($inputs as $input) {
-            $type = (empty($input['type'])) ? $default_input_type : $input['type'];
-            $name = (empty($input['name'])) ? '' : $input['name'];
-            $id = (empty($input['id'])) ? '' : $input['id'];
-            $title = (empty($input['title'])) ? '' : $input['title'];
-            $placeholder = (empty($input['placeholder'])) ? '' : $input['placeholder'];
-            $maxlength = (empty($input['maxlength'])) ? $default_max_length : $input['maxlength'];
-            $classes = (empty($input['classes'])) ? '' : $input['classes'];
-            $autocomplete = (empty($input['autocomplete'])) ? 'on' : $input['autocomplete'];
-            $required = (empty($input['required'])) ? '' : 'required';
-            $value = (empty($input['value'])) ? '' : $input['value'];
-            $disabled = (empty($input['disabled'])) ? '' : $input['disabled'];
+    public static function build_select(array $select) :string {
+        $html = '';
+        $html .= '<select ';
+        $html .= empty($select['name']) ? '' : 'name="' . ApplicationHelper::make_string_safe($select['name']) . '" ';
+        $html .= empty($select['ids']) ? '' : 'id="' . $select['ids'] . '" ';
+        $html .= empty($select['classes']) ? '' : 'class="' . $select['classes'] . '" ';
+        $html .= empty($select['disabled']) ? '' : '' . $select['disabled'] . ' ';
+        $html .= empty($select['required']) ? '' : '' . $select['required'] . ' ';
+        $html .= empty($select['readonly']) ? '' : '' . $select['readonly'] . ' ';
+        $html .= '>';
 
-            $form_string .= "<input type='$type' name='$name' id='$id' title='$title' placeholder='$placeholder' maxlength='$maxlength' class='$classes' autocomplete='$autocomplete' value='$value' $disabled $required/>";
+        foreach ($select['options'] as $option) {
+            $html .= '<option ';
+            $html .= empty($option['value']) ? 'value="0"' : 'value="' . $option['value'] . '" ';
+            $html .= '>';
+            $html .= empty($option['text']) ? 'TEXT' : ApplicationHelper::make_string_safe($option['text']);
         }
 
-        /* Build the buttons */
-        foreach ($buttons as $button) {
-            $type = (empty($button['type'])) ? '' : $button['type'];
-            $button_name = (empty($button['name'])) ? '' : $button['name'];
-            $link = (empty($button['link'])) ? '' : $button['link'];
-            $title = (empty($button['title'])) ? '' : $button['title'];
-            $text = (empty($button['text'])) ? '' : $button['text'];
-            $button_classes = (empty($button['classes'])) ? '' : $button['classes'];
+        $html .= '</select>';
 
-            if ($type === 'button') {
-                $form_string .= "<button name='$button_name' title='$title' class='$button_classes'>$text</button>";
+        return $html;
+    }
+
+    public static function build_button(array $button) :string {
+        $html = '';
+
+        if (strtolower($button['type']) === 'link') {
+            $html .= '<a ';
+            $html .= empty($button['link']) ? '' : 'href="' . $button['link'] . '" ';
+            $html .= empty($button['title']) ? '' : 'title="' . ApplicationHelper::make_string_safe($button['title']) . '" ';
+            $html .= empty($button['ids']) ? '' : 'id="' . $button['ids'] . '" ';
+            $html .= empty($button['classes']) ? '' : 'class="' . $button['classes'] . '" ';
+            $html .= '>';
+            $html .= empty($button['text']) ? 'TEXT' : ApplicationHelper::make_string_safe($button['text']);
+            $html .= '</a>';
+        }
+        elseif (strtolower($button['type']) === 'button') {
+            $html .= '<button ';
+            $html .= empty($button['ids']) ? '' : 'id="' . $button['ids'] . '" ';
+            $html .= empty($button['classes']) ? '' : 'class="' . $button['classes'] . '" ';
+            $html .= '>';
+            $html .= empty($button['text']) ? 'TEXT' : ApplicationHelper::make_string_safe($button['text']);
+            $html .= '</button>';
+        }
+        else {
+            if (ApplicationHelper::is_development()) {
+                echo 'Error: ' . LANG['DevelopmentErrors']['FormHelper']['BuildButton']['UnknownType'];
+                exit();
             }
-            elseif ($type === 'link') {
-                $form_string .= "<a href='$link' name='$name' title='$title' class='$button_classes'>$text</a>";
+            else {
+                ApplicationHelper::redirect_to_error();
+                exit();
             }
         }
 
-        $form_string .= '</form>';
+        return $html;
+    }
 
-        return $form_string;
+    public static function build_label(array $label) :string {
+        $html = '';
+        $html .= '<label ';
+        $html .= empty($label['for']) ? '' : 'for="' . $label['for'] . '" ';
+        $html .= empty($label['ids']) ? '' : 'id="' . $label['ids'] . '" ';
+        $html .= empty($label['classes']) ? '' : 'class="' . $label['classes'] . '" ';
+        $html .= '>';
+        $html .= empty($label['text']) ? 'LABEL TEXT' : ApplicationHelper::make_string_safe($label['text']);
+        $html .= '</label>';
+
+        return $html;
     }
 }
